b637a4c005035a3ce417bdd746c7cb2545bfcc8e
IshDeshpa <ishdeshpa@utexas.edu>
2023-12-03 23:26:47 -0600

    install script updates

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ python -m pip install -e .
 
 cd ..
 mkdir $SCRATCH/ckpt/
-wget https://huggingface.co/Iceclear/StableSR/resolve/main/stablesr_000117.ckpt -P $SCRATCH/ckpt
-wget https://huggingface.co/Iceclear/StableSR/resolve/main/vqgan_cfw_00011.ckpt -P $SCRATCH/ckpt
+if test -f $SCRATCH/ckpt/stablesr_000117.ckpt; then wget https://huggingface.co/Iceclear/StableSR/resolve/main/stablesr_000117.ckpt -P $SCRATCH/ckpt; fi
+if test -f $SCRATCH/ckpt/vqgan_cfw_00011.ckpt; then wget https://huggingface.co/Iceclear/StableSR/resolve/main/vqgan_cfw_00011.ckpt -P $SCRATCH/ckpt; fi
 
 cd ..
 
@@ -37,5 +37,31 @@ source $SCRATCH/miniconda3/etc/profile.d/conda.sh
 conda activate microsoft
 conda install pip
 conda install -c conda-forge dlib
+
+cd Face_Enhancement/models/networks/
+git clone https://github.com/vacancy/Synchronized-BatchNorm-PyTorch
+cp -rf Synchronized-BatchNorm-PyTorch/sync_batchnorm .
+cd ../../../
+
+cd Global/detection_models
+git clone https://github.com/vacancy/Synchronized-BatchNorm-PyTorch
+cp -rf Synchronized-BatchNorm-PyTorch/sync_batchnorm .
+cd ../../
+
+cd Face_Detection/
+wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
+cd ../
+
+cd Face_Enhancement/
+wget https://github.com/microsoft/Bringing-Old-Photos-Back-to-Life/releases/download/v1.0/face_checkpoints.zip
+unzip face_checkpoints.zip
+cd ../
+cd Global/
+wget https://github.com/microsoft/Bringing-Old-Photos-Back-to-Life/releases/download/v1.0/global_checkpoints.zip
+unzip global_checkpoints.zip
+cd ../
+
 python -m pip install -r requirements.txt
+
 cd ../../
