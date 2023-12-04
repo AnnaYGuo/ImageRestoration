01c0fb130f71e87901a6765b3dc91b083c8880cb
IshDeshpa <ishdeshpa@utexas.edu>
2023-12-04 13:46:17 -0600

    more install script stuff

--- GFPGAN/install.sh
+++ GFPGAN/install.sh
@@ -21,4 +21,6 @@ python -m pip install -r requirements.txt
 python setup.py develop
 python -m pip install realesrgan
 
+wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth -P experiments/pretrained_models
+
 cd ..
