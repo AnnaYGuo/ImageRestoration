da953a6a3a74f611eb551bfcae88eb205e8b8db3
IshDeshpa <ishdeshpa@utexas.edu>
2023-12-04 13:39:30 -0600

    changed some structure, split install script

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
-# Install GFPGAN
-cd GFPGAN/GFPGAN
-if conda env list | grep -q gfpgan; 
-then echo "env already exists";
-else conda create --name gfpgan; fi
-
-source $SCRATCH/miniconda3/etc/profile.d/conda.sh
-conda activate gfpgan
-conda install pip
-python -m pip install basicsr facexlib
-python -m pip install -r requirements.txt
-python setup.py develop
-python -m pip install realesrgan
+#!/bin/bash
+set -e
+git submodule update --init --recursive
+conda update conda
 
 # Install base packages
 source $SCRATCH/miniconda3/etc/profile.d/conda.sh
